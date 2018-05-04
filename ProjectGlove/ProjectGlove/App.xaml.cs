f23fafd9ea9aaab5c4b008684ae2d741df0628e1
Andrew Dolan <andrewdolan@hotmail.co.nz>
2018-05-05 02:14:35 +1200

    Integrated Microsoft App Center Analytics and Crashes. Something that can work nicely with the CI of this app.

--- ProjectGlove/ProjectGlove/App.xaml.cs
+++ ProjectGlove/ProjectGlove/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 using Xamarin.Forms;
 
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
 namespace ProjectGlove
 {
 	public partial class App : Application
@@ -18,8 +22,11 @@ namespace ProjectGlove
 
 		protected override void OnStart ()
 		{
-			// Handle when your app starts
-		}
+            AppCenter.Start("android=9abdcd14-b353-404f-b455-95ddbb149fa5;"
+                            + "uwp=4ca0cedc-338a-4182-85d1-18a7a2c430f7;"
+                            + "ios=7e59dde8-3cb9-4f56-b2dd-a50224f2af5b",
+                            typeof(Analytics), typeof(Crashes));
+        }
 
 		protected override void OnSleep ()
 		{
